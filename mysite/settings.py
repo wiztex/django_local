c885f58a0c7a153f3567206e2b4a0695d9b9a79a
Wei Lou <wlou05@gmail.com>
2018-03-22 18:58:32 -0400

    v4

--- mysite/settings.py
+++ mysite/settings.py
@@ -83,14 +83,20 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 #     }
 # }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'django',
+#         'USER': 'root',
+#         'PASSWORD': 'business',
+#         'HOST': 'localhost',
+#         'PORT': ''
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django',
-        'USER': 'root',
-        'PASSWORD': 'business',
-        'HOST': 'localhost',
-        'PORT': ''
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
