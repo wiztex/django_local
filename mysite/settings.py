b698b12bce6af4d1ed71669c1fe95beda199e968
Wei Lou <wlou05@gmail.com>
2018-03-22 19:50:51 -0400

    v4

--- mysite/settings.py
+++ mysite/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'u-#0fpsuold@ks-60&p25oz5qvq)c(0)h(*d7_j+72swtcb!)$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
